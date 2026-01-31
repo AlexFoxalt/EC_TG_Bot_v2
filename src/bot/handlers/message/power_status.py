836209594e9a1ddd0119f5e6201f9db6ae8b4f58
Oleksii Chernyshov <oleksii_chernyshov@epam.com>
2026-01-31 12:17:36 +0200

    fix too long sql session usage

--- src/bot/handlers/message/power_status.py
+++ src/bot/handlers/message/power_status.py
@@ -45,48 +45,48 @@ async def handle_power_status(update: Update, context: ContextTypes.DEFAULT_TYPE
             f"Retrieved latest [{power_label}] status value={latest_status.value}, date_created={latest_status.date_created.isoformat()}"
         )
 
-        if latest_status is None:
-            logger.bind(username=u_identity).warning(f"No status Status record with label [{power_label}] found in DB")
-            await update.message.reply_text(
-                langpack.MSG_POWER_STATUS_NOT_AVAILABLE,
-                reply_markup=get_main_keyboard(langpack),
-            )
-            return
-
-        curr_time = datetime.now(UTC)
-        time_diff = int((curr_time - latest_status.date_created).total_seconds())
-
-        # Determine status message
-        is_on = latest_status.value
-        status_text = langpack.MSG_POWER_IS_ON if is_on else langpack.MSG_POWER_IS_OFF
-        datetime_text = langpack.MSG_POWER_TURN_ON_TIME if is_on else langpack.MSG_POWER_TURN_OFF_TIME
-        date_created_timezone = latest_status.date_created.astimezone(KYIV_TZ)
-        time_diff_text = ""
-        if time_diff:
-            diff_mins = time_diff // SECS_IN_MINUTE
-            if diff_mins > MINS_IN_HOUR:
-                # Convert minutes to hours if it's possible.
-                # "100500 mins elapsed" message looks weird.
-                diff_hours = diff_mins // MINS_IN_HOUR
-                diff_mins = diff_mins % MINS_IN_HOUR
-
-                if diff_mins == 0:
-                    text_suffix = f"{diff_hours} {langpack.WORD_HOURS}\\."
-                else:
-                    text_suffix = f"{diff_hours} {langpack.WORD_HOURS}\\. {langpack.WORD_AND} {diff_mins} {langpack.WORD_MINUTES}\\."
-            else:
-                text_suffix = f"{diff_mins} {langpack.WORD_MINUTES}\\."
-
-            if latest_status.value:
-                time_diff_text = f"{langpack.MSG_TIME_SINCE_POWER_ON}\n{text_suffix}"
-            elif not latest_status.value:
-                time_diff_text = f"{langpack.MSG_TIME_SINCE_SHUTDOWN}\n{text_suffix}"
-
-        final_text = (
-            f"{status_text}\n\n{datetime_text}\n{date_created_timezone:%H:%M %d\\.%m\\.%Y }\n\n{time_diff_text}"
-        )
+    if latest_status is None:
+        logger.bind(username=u_identity).warning(f"No status Status record with label [{power_label}] found in DB")
         await update.message.reply_text(
-            final_text,
+            langpack.MSG_POWER_STATUS_NOT_AVAILABLE,
             reply_markup=get_main_keyboard(langpack),
-            parse_mode=ParseMode.MARKDOWN_V2,
         )
+        return
+
+    curr_time = datetime.now(UTC)
+    time_diff = int((curr_time - latest_status.date_created).total_seconds())
+
+    # Determine status message
+    is_on = latest_status.value
+    status_text = langpack.MSG_POWER_IS_ON if is_on else langpack.MSG_POWER_IS_OFF
+    datetime_text = langpack.MSG_POWER_TURN_ON_TIME if is_on else langpack.MSG_POWER_TURN_OFF_TIME
+    date_created_timezone = latest_status.date_created.astimezone(KYIV_TZ)
+    time_diff_text = ""
+    if time_diff:
+        diff_mins = time_diff // SECS_IN_MINUTE
+        if diff_mins > MINS_IN_HOUR:
+            # Convert minutes to hours if it's possible.
+            # "100500 mins elapsed" message looks weird.
+            diff_hours = diff_mins // MINS_IN_HOUR
+            diff_mins = diff_mins % MINS_IN_HOUR
+
+            if diff_mins == 0:
+                text_suffix = f"{diff_hours} {langpack.WORD_HOURS}\\."
+            else:
+                text_suffix = (
+                    f"{diff_hours} {langpack.WORD_HOURS}\\. {langpack.WORD_AND} {diff_mins} {langpack.WORD_MINUTES}\\."
+                )
+        else:
+            text_suffix = f"{diff_mins} {langpack.WORD_MINUTES}\\."
+
+        if latest_status.value:
+            time_diff_text = f"{langpack.MSG_TIME_SINCE_POWER_ON}\n{text_suffix}"
+        elif not latest_status.value:
+            time_diff_text = f"{langpack.MSG_TIME_SINCE_SHUTDOWN}\n{text_suffix}"
+
+    final_text = f"{status_text}\n\n{datetime_text}\n{date_created_timezone:%H:%M %d\\.%m\\.%Y }\n\n{time_diff_text}"
+    await update.message.reply_text(
+        final_text,
+        reply_markup=get_main_keyboard(langpack),
+        parse_mode=ParseMode.MARKDOWN_V2,
+    )
