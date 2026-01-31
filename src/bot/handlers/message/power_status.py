6d4bd1c660843a26464c9df6638cd75badd1136c
Oleksii Chernyshov <oleksii_chernyshov@epam.com>
2026-01-31 12:10:07 +0200

    added 'time passed since...' to power status

--- src/bot/handlers/message/power_status.py
+++ src/bot/handlers/message/power_status.py
@@ -1,3 +1,5 @@
+from datetime import datetime, UTC
+
 from sqlalchemy import select, desc
 from telegram import Update
 from telegram.constants import ParseMode
@@ -5,7 +7,7 @@ from telegram.ext import (
     ContextTypes,
 )
 
-from src.bot.constants import KYIV_TZ
+from src.bot.constants import KYIV_TZ, SECS_IN_MINUTE, MINS_IN_HOUR
 from src.bot.keyboards import get_main_keyboard
 from src.bot.lang_pack.base import BaseLangPack
 from src.bot.utils import get_user_identity_from_update
@@ -39,6 +41,9 @@ async def handle_power_status(update: Update, context: ContextTypes.DEFAULT_TYPE
             select(Status).where(Status.label == power_label).order_by(desc(Status.date_created)).limit(1)
         )
         latest_status = result.scalar_one_or_none()
+        logger.bind(username=u_identity).info(
+            f"Retrieved latest [{power_label}] status value={latest_status.value}, date_created={latest_status.date_created.isoformat()}"
+        )
 
         if latest_status is None:
             logger.bind(username=u_identity).warning(f"No status Status record with label [{power_label}] found in DB")
@@ -48,17 +53,40 @@ async def handle_power_status(update: Update, context: ContextTypes.DEFAULT_TYPE
             )
             return
 
+        curr_time = datetime.now(UTC)
+        time_diff = int((curr_time - latest_status.date_created).total_seconds())
+
         # Determine status message
         is_on = latest_status.value
         status_text = langpack.MSG_POWER_IS_ON if is_on else langpack.MSG_POWER_IS_OFF
         datetime_text = langpack.MSG_POWER_TURN_ON_TIME if is_on else langpack.MSG_POWER_TURN_OFF_TIME
         date_created_timezone = latest_status.date_created.astimezone(KYIV_TZ)
-        logger.bind(username=u_identity).info(
-            f"Retrieved latest [{power_label}] status value={is_on}, date_created={latest_status.date_created.isoformat()}"
-        )
+        time_diff_text = ""
+        if time_diff:
+            diff_mins = time_diff // SECS_IN_MINUTE
+            if diff_mins > MINS_IN_HOUR:
+                # Convert minutes to hours if it's possible.
+                # "100500 mins elapsed" message looks weird.
+                diff_hours = diff_mins // MINS_IN_HOUR
+                diff_mins = diff_mins % MINS_IN_HOUR
 
+                if diff_mins == 0:
+                    text_suffix = f"{diff_hours} {langpack.WORD_HOURS}\\."
+                else:
+                    text_suffix = f"{diff_hours} {langpack.WORD_HOURS}\\. {langpack.WORD_AND} {diff_mins} {langpack.WORD_MINUTES}\\."
+            else:
+                text_suffix = f"{diff_mins} {langpack.WORD_MINUTES}\\."
+
+            if latest_status.value:
+                time_diff_text = f"{langpack.MSG_TIME_SINCE_POWER_ON}\n{text_suffix}"
+            elif not latest_status.value:
+                time_diff_text = f"{langpack.MSG_TIME_SINCE_SHUTDOWN}\n{text_suffix}"
+
+        final_text = (
+            f"{status_text}\n\n{datetime_text}\n{date_created_timezone:%H:%M %d\\.%m\\.%Y }\n\n{time_diff_text}"
+        )
         await update.message.reply_text(
-            f"{status_text}\n\n{datetime_text}{date_created_timezone:%H:%M %d\\.%m\\.%Y }",
+            final_text,
             reply_markup=get_main_keyboard(langpack),
             parse_mode=ParseMode.MARKDOWN_V2,
         )
