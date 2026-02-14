5dfd356bb6e1e2d0ea748106b70b77ee35704284
Oleksii Chernyshov <oleksii_chernyshov@epam.com>
2026-02-14 17:23:15 +0200

    fix: upd setting btn color

--- src/bot/keyboards.py
+++ src/bot/keyboards.py
@@ -6,10 +6,10 @@ from src.bot.lang_pack.base import BaseLangPack
 def get_main_keyboard(langpack: BaseLangPack) -> ReplyKeyboardMarkup:
     """Get the main persistent keyboard for registered users."""
     keyboard = [
-        [KeyboardButton(langpack.BTN_POWER_STATUS, api_kwargs={"style": "primary"})],
-        [KeyboardButton(langpack.BTN_GEN_STATUS, api_kwargs={"style": "primary"})],
+        [KeyboardButton(langpack.BTN_POWER_STATUS, api_kwargs={"style": "success"})],
+        [KeyboardButton(langpack.BTN_GEN_STATUS, api_kwargs={"style": "success"})],
         [
-            KeyboardButton(langpack.BTN_SETTINGS, api_kwargs={"style": "success"}),
+            KeyboardButton(langpack.BTN_SETTINGS),
             KeyboardButton(langpack.BTN_REPORT_ERROR, api_kwargs={"style": "danger"}),
         ],
     ]
